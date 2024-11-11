fb32cf917320ffccb2a6c577f3b3a647381dbb8c
Franco <aesevv@gmail.com>
2024-11-11 11:21:31 -0300

    agregando validacion si no se sube ninguna imagen

--- src/controllers/multerController.js
+++ src/controllers/multerController.js
@@ -10,36 +10,41 @@ const ProbandoMulter = {
         console.log(req.file);
         console.log("- - - - - - - - - - - - Datos de la imagen - - - - - - - - - - - - ");
 
+        function GuardarEnJson(datosImagen_1) {
+            // Ruta completa del archivo JSON
+            const filePath = path.join(__dirname, "../data/datos.json");
+
+            // Traer el archivo JSON
+            let archivoJson = fs.readFileSync(filePath, "utf8");
+            // Convertir el archivo JSON a un objeto literal
+            let datosJsonAjs = JSON.parse(archivoJson);
+            // Agregar los nuevos datos al objeto existente
+            datosJsonAjs.push(datosImagen_1);
+            // Convertir el objeto literal a un string JSON
+            let datosJsonAjsString = JSON.stringify(datosJsonAjs, null, 4);
+            // Escribir el archivo JSON actualizado
+            fs.writeFileSync(filePath, datosJsonAjsString);
+            // Traer el archivo JSON
+            let NuevoArchivoJson = fs.readFileSync(filePath, "utf8");
+            // Convertir el archivo JSON a un objeto literal
+            let datosJsonAjsActualizado = JSON.parse(NuevoArchivoJson);
+            console.log("- - - - - - - - - - - - Datos de la JSON - - - - - - - - - - - - ");
+            //console.log(datosJsonAjsActualizado);
+            return datosJsonAjsActualizado
+        }
+
         let datos = req.body;
-        let imagenNomnbre = req.file.filename;
+        let imagenNomnbre = req.file ? req.file.filename : "default.webp";
         let datosImagen = {
             nombre: datos.nombre,
             precio: datos.precio,
             NombreImagen: imagenNomnbre
         };
-        console.log(datosImagen);
+        ;
 
-        // Ruta completa del archivo JSON
-        const filePath = path.join(__dirname, "../data/datos.json");
+        res.render("../views/products/multerVista", { "datos": GuardarEnJson(datosImagen) });
 
-        // Traer el archivo JSON
-        let archivoJson = fs.readFileSync(filePath, "utf8");
-        // Convertir el archivo JSON a un objeto literal
-        let datosJsonAjs = JSON.parse(archivoJson);
-        // Agregar los nuevos datos al objeto existente
-        datosJsonAjs.push(datosImagen);
-        // Convertir el objeto literal a un string JSON
-        let datosJsonAjsString = JSON.stringify(datosJsonAjs, null, 4);
-        // Escribir el archivo JSON actualizado
-        fs.writeFileSync(filePath, datosJsonAjsString);
-        // Traer el archivo JSON
-        let NuevoArchivoJson = fs.readFileSync(filePath, "utf8");
-        // Convertir el archivo JSON a un objeto literal
-        let datosJsonAjsActualizado = JSON.parse(NuevoArchivoJson);
-        console.log("- - - - - - - - - - - - Datos de la JSON - - - - - - - - - - - - ");
-        //console.log(datosJsonAjsActualizado);
 
-        res.render("../views/products/multerVista", { "datos": datosJsonAjsActualizado });
     }
 };
 
